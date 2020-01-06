85b410133408b9ed67f8240aedd68548bf9f9433
chencanyi <chency1997@ruc.edu.cn>
2020-01-07 01:30:44 +0800

    modify demo

--- demo/semicuredemo.R
+++ demo/semicuredemo.R
@@ -1,14 +1,4 @@
 # Reproduce table 1
 set.seed(47)
-for(gamma in seq(0,1,length.out = 5)){
-  for(n in c(200,400)){
-    reptable(gamma = gamma, n = n,rep = 1000,clusterN = 100 )
-  }
-}
+reptable(rep = 8, clusterN = 8 )
 
-# Reproduce table 2
-for(gamma in seq(0,1,length.out = 3)){
-  for(n in c(400)){
-    reptable(gamma = gamma, n = n,rep = 1000,clusterN = 100 )
-  }
-}
